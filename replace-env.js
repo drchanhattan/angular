f3092d17661682fc6da95c5b7781da3d5303776d
Chris Chan <chrisman_78@hotmai.co.uk>
2024-07-19 17:12:02 +0100

    commit

---
+++ replace-env.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// Define the environment variables
+const envVariables = {
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+};
+
+// Define the path to the environment file
+const envFilePath = path.join(__dirname, 'src/environments/environment.prod.ts');
+
+// Read the file
+let fileContent = fs.readFileSync(envFilePath, 'utf8');
+
+// Replace the placeholders
+for (const [key, value] of Object.entries(envVariables)) {
+  const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
+  fileContent = fileContent.replace(regex, value);
+}
+
+// Write the modified file back
+fs.writeFileSync(envFilePath, fileContent, 'utf8');
+
+console.log('Environment variables have been replaced.');
