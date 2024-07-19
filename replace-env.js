56af983ccc96916c5f0d1c4c084bc13be05db2f8
Chris Chan <chrisman_78@hotmai.co.uk>
2024-07-19 17:39:47 +0100

    commit

--- replace-env.js
+++ replace-env.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 // Define the environment variables
 const envVariables = {
-  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  FIREBASE_API_KEY: process.env['FIREBASE_API_KEY'],
+  FIREBASE_AUTH_DOMAIN: process.env['FIREBASE_AUTH_DOMAIN'],
+  FIREBASE_PROJECT_ID: process.env['FIREBASE_PROJECT_ID'],
+  FIREBASE_STORAGE_BUCKET: process.env['FIREBASE_STORAGE_BUCKET'],
+  FIREBASE_MESSAGING_SENDER_ID: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  FIREBASE_APP_ID: process.env['FIREBASE_APP_ID'],
 };
 
 // Define the path to the environment file
@@ -27,3 +27,4 @@ for (const [key, value] of Object.entries(envVariables)) {
 fs.writeFileSync(envFilePath, fileContent, 'utf8');
 
 console.log('Environment variables have been replaced.');
+console.log(process.env['FIREBASE_PROJECT_ID']);
