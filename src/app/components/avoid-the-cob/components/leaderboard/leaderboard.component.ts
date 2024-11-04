c083ebc83d10be735550835d4b7d35aab2156758
Chris <chrisman_78@hotmail.co.uk>
2024-11-04 17:00:14 +0000

    commit

--- src/app/components/avoid-the-cob/components/leaderboard/leaderboard.component.ts
+++ src/app/components/avoid-the-cob/components/leaderboard/leaderboard.component.ts
@@ -1,15 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, HostBinding } from '@angular/core';
 import { GameButtonComponent } from '../game-button/game-button.component';
+import { PlayerNameService } from '../player-name/player-name.service';
 import { LeaderboardService } from './leaderboard.service';
 
 @Component({
   selector: 'app-leaderboard',
   standalone: true,
-  imports: [GameButtonComponent],
+  imports: [CommonModule, GameButtonComponent],
   templateUrl: './leaderboard.component.html',
 })
 export class LeaderboardComponent {
   @HostBinding('class') hostClasses = 'absolute flex size-full flex-col items-center justify-center';
 
-  constructor(public leaderboardService: LeaderboardService) {}
+  constructor(
+    public leaderboardService: LeaderboardService,
+    public nameService: PlayerNameService,
+  ) {}
 }
